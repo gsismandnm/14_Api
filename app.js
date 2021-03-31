b1850292159b54518af12320dad038fa8fabf10c
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-03-31 23:41:53 +0300

    Last Update

--- app.js
+++ app.js
@@ -71,3 +71,55 @@ const kayitSil = axios.delete(fetch('https://jsonplaceholder.typicode.com/todos/
 
 kayitSil.then(responce => console.log(responce));
 console.log(kayitSil)
+
+
+sql
+*/
+
+
+// fetch('https://jsonplaceholder.typicode.com/users/')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+
+// const users = axios.get('https://jsonplaceholder.typicode.com/users/');
+
+// users.then(response => console.log(response));
+
+
+// const yeniKayit = axios.post('https://jsonplaceholder.typicode.com/todos/',
+//     {
+//         "userId": 10,
+//         "title": "yeni todo",
+//         "completed": true
+//     }
+// );
+
+// yeniKayit.then(response => console.log(response));
+
+// console.log(yeniKayit)
+
+
+// const kayitDegistir = axios.put('https://jsonplaceholder.typicode.com/todos/1',
+//     {
+//         "userId": 4,
+//         "title": "degistirildi",
+//         "completed": true
+//     }
+// );
+
+// kayitDegistir.then(response => console.log(response));
+
+
+// const kayitSil = axios.delete('https://jsonplaceholder.typicode.com/todos/1');
+
+// kayitSil.then(response => console.log(response));
+
+
+const kayitKismenDegistir = axios.patch('https://jsonplaceholder.typicode.com/todos/1',
+    {
+        "title": "kismen degistirildi",
+    }
+);
+
+kayitKismenDegistir.then(response => console.log(response));
